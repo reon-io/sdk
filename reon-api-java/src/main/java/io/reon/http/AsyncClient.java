675a4830527c0faf5f7ac94da1862d97e28d72c5
Jacek Zadrąg <jacek.zadrag@javeo.eu>
2015-11-19 18:20:52 +0100

    Async client

---
+++ reon-api-java/src/main/java/io/reon/http/AsyncClient.java
@@ -0,0 +1,51 @@
+package io.reon.http;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+
+import io.reon.concurrent.ListenableExecutorService;
+import io.reon.concurrent.ListenableFuture;
+
+public class AsyncClient implements Closeable {
+
+	private final io.reon.net.Connection conn;
+	private final ListenableExecutorService executorService;
+	private final MessageWriter writer;
+	private final ResponseReader reader;
+
+	public AsyncClient(io.reon.net.Connection conn, ExecutorService executorService) throws IOException {
+		this.conn = conn;
+		this.executorService = new ListenableExecutorService(executorService);
+		writer = new MessageWriter(conn.getOutputStream());
+		reader = new ResponseReader(conn.getInputStream());
+	}
+
+	private void write(Request r) throws IOException {
+		synchronized(writer) {
+			writer.write(r);
+		}
+	}
+
+	private Response read() throws IOException {
+		synchronized (reader) {
+			return reader.read();
+		}
+	}
+
+	public ListenableFuture<Response> send(final Request request) {
+		return executorService.submit(new Callable<Response>() {
+			@Override
+			public Response call() throws Exception {
+				write(request);
+				return read();
+			}
+		});
+	}
+
+	@Override
+	public void close() throws IOException {
+		conn.close();
+	}
+}
